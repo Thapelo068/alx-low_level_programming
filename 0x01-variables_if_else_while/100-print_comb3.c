cf8189f2906aa7ceb68d1609d667c0cc36b48b54
Thapelo068 <thapelomamiane@gmail.com>
2023-03-19 00:29:47 -0700

    0x01. C - Variables, if, else, while with  advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	int d, p;
-	for (d = '0'; d < '9'; d++)
-	{
+	int n;
 
-	for (p = d +1; p<= '9'; p++)
+	for (n = 48; n < 58; n++)
 	{
-	if (p != d)
-	{
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p == '9')
-	}
-	}
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
